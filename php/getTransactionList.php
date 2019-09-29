c885909d730b52c16b46bc3017ca3e02580db24c
Arifin Rais <13517067@stei.itb.ac.id>
2019-09-29 07:54:06 +0700

    still unfinished

---
+++ php/getTransactionList.php
@@ -0,0 +1,21 @@
+<?php
+    include('dbAccess.php');
+    
+    $db = new DatabaseAccess("localhost", "root", "", "wbd");
+
+    //kumaha ieu skemana??
+    $result = $db->query(
+       "SELECT film_id, title, film_picture, film_schedule as avg_rating
+        FROM film NATURAL JOIN schedule NATURAL JOIN transactions
+        AS film(film_id, title, film_picture, film_schedule)"
+    );
+    if($result){
+        $res = array();
+        while($data = $result->fetch_assoc()) {
+            array_push($res, $data);
+        }
+        echo json_encode($res);
+    }else{
+        echo json_encode("");
+    }
+?>
